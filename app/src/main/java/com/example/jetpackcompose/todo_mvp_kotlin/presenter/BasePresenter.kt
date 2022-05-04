c6f54e147113402ba8fca9b2750c4db156b45644
ogata-k <ogtkzk712@gmail.com>
2022-05-04 17:34:30 +0900

    initial commit

---
+++ app/src/main/java/com/example/jetpackcompose/todo_mvp_kotlin/presenter/BasePresenter.kt
@@ -0,0 +1,5 @@
+package com.example.jetpackcompose.todo_mvp_kotlin.presenter
+
+interface BasePresenter {
+    fun start()
+}
---
+++ app/src/main/java/com/example/jetpackcompose/todo_mvp_kotlin/view/BaseView.kt
@@ -0,0 +1,5 @@
+package com.example.jetpackcompose.todo_mvp_kotlin.view
+
+interface BaseView<P> {
+    var presenter: P
+}
