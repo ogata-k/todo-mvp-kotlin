9a021ac0659a00bb88dde08db42c02640a79295a
ogata-k <ogtkzk712@gmail.com>
2022-05-04 17:35:16 +0900

    add model

---
+++ app/src/main/java/com/example/jetpackcompose/todo_mvp_kotlin/model/Task.kt
@@ -0,0 +1,14 @@
+package com.example.jetpackcompose.todo_mvp_kotlin.model
+
+import java.util.*
+
+data class Task(val title: String ="", val description: String = "", val id: String = UUID.randomUUID().toString(), val isCompleted: Boolean = false){
+    val titleForList: String
+        get() = if (title.isNotEmpty()) title else description
+
+    val isActive: Boolean
+        get() = !isCompleted
+
+    val isEmpty: Boolean
+        get() = title.isEmpty() && description.isEmpty()
+}
