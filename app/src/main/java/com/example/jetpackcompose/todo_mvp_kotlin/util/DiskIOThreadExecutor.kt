c089945559c9a41aefec552999655612b35dec06
ogata-k <ogtkzk712@gmail.com>
2022-05-04 20:35:40 +0900

    スレッドごとに実行できるヘルパーを作成

---
+++ app/src/main/java/com/example/jetpackcompose/todo_mvp_kotlin/util/DiskIOThreadExecutor.kt
@@ -0,0 +1,13 @@
+package com.example.jetpackcompose.todo_mvp_kotlin.util
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+/**
+ * Executor that runs a task on a new background thread.
+ */
+class DiskIOThreadExecutor : Executor {
+    private val diskIO = Executors.newSingleThreadExecutor()
+
+    override fun execute(command: Runnable) { diskIO.execute(command) }
+}
